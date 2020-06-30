846b007e7737a23c968d09d278de95a0addc0403
Grainster Dima <48991276+dimutch833@users.noreply.github.com>
2020-06-30 17:48:24 +0300

    Fuschia Smalt - 0.2

--- Hismic2-Core/src/BUILD.h
+++ Hismic2-Core/src/BUILD.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define NAME "HismicEngine2"
-#define VERSION "0.2-r2"
+#define VERSION "0.2-r3"
 #define BUILD_DATE __DATE__ " " __TIME__
 #define CODENAME "Fuschia Smalt"
 #define DEVELOPER "Grainster Softworks"
