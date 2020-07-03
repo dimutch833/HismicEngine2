40312707e435f99546efb8812bb570d2ee23ef6e
Grainster Dima <48991276+dimutch833@users.noreply.github.com>
2020-07-03 19:46:20 +0300

    Added Vec2

---
+++ Hismic2-Core/src/maths/vec2.cpp
@@ -0,0 +1,47 @@
+#include "vec2.h"
+
+namespace hismic {
+	namespace maths {
+	
+	vec2::vec2() {
+
+		x = 0.0f;
+		y = 0.0f;
+	}
+	vec2::vec2(const float& x, const float& y) {
+		this->x = x;
+		this->y = y;
+
+	}
+
+	vec2& vec2::add(const vec2& other)
+	{
+		x += other.x;
+		y += other.y;
+		return *this;
+	}
+
+	vec2& vec2::subtract(const vec2& other)
+	{
+		x -= other.x;
+		y -= other.y;
+		return *this;
+	}
+
+	vec2& vec2::multiply(const vec2& other)
+	{
+		x *= other.x;
+		y *= other.y;
+		return *this;
+	}
+
+	vec2& vec2::divide(const vec2& other)
+	{
+		x /= other.x;
+		y /= other.y;
+		return *this;
+	}
+	
+	
+	}
+}
--- Hismic2-Core/src/maths/vec2.h
+++ Hismic2-Core/src/maths/vec2.h
@@ -1,1 +1,22 @@
 #pragma once
+
+namespace hismic {
+	namespace maths {
+
+		struct vec2 {
+
+			float x, y;
+
+			vec2();
+			vec2(const float& x, const float& y);
+
+			vec2& add(const vec2& other);
+			vec2& subtract(const vec2& other);
+			vec2& multiply(const vec2& other);
+			vec2& divide(const vec2& other);
+
+		};
+
+
+ } 
+}
