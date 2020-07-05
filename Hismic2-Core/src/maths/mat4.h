6a48ee4ffa7fc98873d66e2ef293a1bdcdbdd731
Grainster Dima <48991276+dimutch833@users.noreply.github.com>
2020-07-05 20:23:45 +0300

    mat4

--- Hismic2-Core/src/maths/mat4.h
+++ Hismic2-Core/src/maths/mat4.h
@@ -1,5 +1,8 @@
 #pragma once
+
+#define _USE_MATH_DEFINES
 #include "maths.h"
+#include <math.h>
 
 namespace hismic {
 	namespace maths {
@@ -13,7 +16,7 @@ namespace hismic {
 			static mat4 identity();
 
 			mat4& multiply(const mat4& other);
-			friend mat4 operator*(mat4 left, const mat4& right);
+			friend mat4 operator*(mat4& left, const mat4& right);
 			mat4& operator*=(const mat4& other);
 
 			static mat4 orthographic(float left,float right,float bottom,float top,float near,float far);
@@ -22,8 +25,6 @@ namespace hismic {
 			static mat4 translation(const vec3& translation);
 			static mat4 rotation(float angle,const vec3 axis);
 			static mat4 scale(const vec3& scale);
-
-
 		};
 
 	}
--- Hismic2-Core/src/maths/maths.h
+++ Hismic2-Core/src/maths/maths.h
@@ -3,3 +3,8 @@
 #include "vec2.h"
 #include "vec3.h"
 #include "vec4.h"
+
+
+float ToRadians(float degrees) {
+	return degrees * (M_PI / 180.0f);
+}
