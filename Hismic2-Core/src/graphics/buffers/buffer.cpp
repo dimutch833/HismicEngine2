bc5cc5e119e1a60ba407202b1c0fc1d548d03de9
Grainster Dima <48991276+dimutch833@users.noreply.github.com>
2020-07-31 20:01:38 +0300

    Simple Buffer

--- Hismic2-Core/src/graphics/buffers/buffer.cpp
+++ Hismic2-Core/src/graphics/buffers/buffer.cpp
@@ -1,1 +1,23 @@
 #include "buffer.h"
+
+namespace hismic {
+	namespace graphics {
+		Buffer::Buffer(GLfloat* data, GLsizei count, GLuint componentCount)
+			:m_ComponentCount(componentCount)
+		{
+			glGenBuffers(1, &m_BufferID);
+			glBindBuffer(GL_ARRAY_BUFFER, m_BufferID);
+			glBufferData(GL_ARRAY_BUFFER, count * sizeof(GL_FLOAT), data, GL_STATIC_DRAW);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		}
+		void Buffer::bind() const
+		{
+			glBindBuffer(GL_ARRAY_BUFFER, m_BufferID);
+		}
+		void Buffer::unbind() const
+		{
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+		}
+	}
+}
--- Hismic2-Core/src/graphics/buffers/buffer.h
+++ Hismic2-Core/src/graphics/buffers/buffer.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <GL/glew.h>
+
+namespace hismic {
+	namespace graphics {
+		class Buffer {
+		private:
+			GLuint m_BufferID;
+			GLuint m_ComponentCount;
+		public:
+			Buffer(GLfloat* data,GLsizei count,GLuint componentCount);
+
+			void bind() const;
+			void unbind() const;
+			inline GLuint getComponentCount() { return m_ComponentCount; }
+		};
+	}
+}
