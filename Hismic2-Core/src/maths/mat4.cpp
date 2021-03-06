8edaefe85a0f205ff3b46c7a0a8de99ba2281ef6
Grainster Dima <48991276+dimutch833@users.noreply.github.com>
2021-03-06 16:03:34 +0200

    mat4 fix bug multiplying

--- Hismic2-Core/src/maths/mat4.cpp
+++ Hismic2-Core/src/maths/mat4.cpp
@@ -26,15 +26,18 @@ namespace hismic {
 		}
 		mat4& mat4::multiply(const mat4& other)
 		{
+			float data[16];
+
 			for (int y = 0; y < 4; y++) {
 				for (int x = 0; x < 4; x++) {
 					float sum = 0.0f;
 					for (int e = 0; e < 4; e++) {
 						sum += elements[x + e * 4] * other.elements[e + y * 4];
 					}
-					elements[x + y * 4] = sum;
+					data[x + y * 4] = sum;
 				}
 			}
+			memcpy(elements,data,4 * 4 * sizeof(float));
 			return *this;
 		}
 		mat4& mat4::operator*=(const mat4& other)
